cb006688e127741f84a8eda88b310ad5ef01a113
anav <anam.velezr@uqvirtual.edu.co>
2025-03-13 00:34:32 -0500

    new

--- src/main/java/co/edu/uniquindio/repositories/UserRepository.java
+++ src/main/java/co/edu/uniquindio/repositories/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    boolean existById(String id);
+    boolean existsById(String id);
     Optional<User> findByEmail(String email);
 }
